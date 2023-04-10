865d145d4b7bac7e08fd527c4968a4f51682f87f
Siddharth Kishan <91934415+sidkishan@users.noreply.github.com>
2023-04-10 22:37:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 //your JS code here. If required.
 const input = prompt("Enter a string");
-for(var i = 0; i < input.length; i++){
+function findNonRepeating(input){
+	for(var i = 0; i < input.length; i++){
 	var restInput = input.substring(0, i) + input.substring((i + 1), input.length);
 	if(!restInput.includes(input.charAt(i))) return input.charAt(i);
 }
+}
+
