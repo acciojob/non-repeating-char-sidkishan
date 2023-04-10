96ec696d565aa1d602ae1a208d581e7faa8b430e
Siddharth Kishan <91934415+sidkishan@users.noreply.github.com>
2023-04-10 22:37:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ function findNonRepeating(input){
 	for(var i = 0; i < input.length; i++){
 	var restInput = input.substring(0, i) + input.substring((i + 1), input.length);
 	if(!restInput.includes(input.charAt(i))) return input.charAt(i);
-}
+	}
 }
 
